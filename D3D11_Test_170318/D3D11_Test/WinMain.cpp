56c440a6e3a2ee48becf3510c0467ce6cde046ee
scintil89 <scintil89@nhnnext.org>
2017-05-23 03:11:07 +0900

    070318 update

--- D3D11_Test_170318/D3D11_Test/WinMain.cpp
+++ D3D11_Test_170318/D3D11_Test/WinMain.cpp
@@ -1,38 +1,44 @@
 #include <windows.h>
 #include <time.h>
 
+//////////////////////////////////////////////////////////////////////////
+//DirectX 11 header
 #include <dxgi.h>
 #include <d3d11.h>
 #include <d3dCompiler.h>
 #include <d3dx11.h>
 #include <dxerr.h>
-
 #include <xnamath.h>
+//////////////////////////////////////////////////////////////////////////
 
-#define szWindowClass	TEXT("First")
-#define szTitle			TEXT("First App")
-
-LRESULT CALLBACK WndProc(HWND hWnd
-	, UINT message
-	, WPARAM wParam
-	, LPARAM lParam);
+#define szWindowClass	TEXT("02 IndexBuffer")
+#define szTitle			TEXT("02 IndexBuffer")
 
 struct MyVertex
 {
 	XMFLOAT3	pos;
+	XMFLOAT4	color;
 };
 
 HWND					g_hWnd = NULL;
 IDXGISwapChain*			g_pSwapChain = NULL;
 ID3D11Device*			g_pd3dDevice = NULL;
-ID3D11DeviceContext*	g_pImmediateContex = NULL; //d3dc
+ID3D11DeviceContext*	g_pImmediateContext = NULL; //d3dc
 ID3D11RenderTargetView*	g_pRenderTargetView = NULL;
 D3D_FEATURE_LEVEL		g_featureLevel = D3D_FEATURE_LEVEL_11_0;
 
 ID3D11VertexShader*		g_pVertexShader = NULL;
 ID3D11InputLayout*		g_pVertexLayout = NULL;
 ID3D11PixelShader*		g_pPixelShader = NULL;
-ID3D11Buffer*			g_pVertexBuffer;
+ID3D11Buffer*			g_pVertexBuffer = NULL;
+ID3D11Buffer*			g_pIndexBuffer = NULL;
+
+LRESULT CALLBACK WndProc(
+	HWND hWnd,
+	UINT message,
+	WPARAM wParam,
+	LPARAM lParam
+	);
 
 HRESULT initDevice()
 {
@@ -47,12 +53,12 @@ HRESULT initDevice()
 
 	D3D_FEATURE_LEVEL	featureLevels[] =
 	{
-		D3D_FEATURE_LEVEL_11_0, 
-		D3D_FEATURE_LEVEL_10_1, 
+		D3D_FEATURE_LEVEL_11_0,
+		D3D_FEATURE_LEVEL_10_1,
 		D3D_FEATURE_LEVEL_10_0
 	};
 	UINT numFeatureLevels = ARRAYSIZE(featureLevels);
-	
+
 	DXGI_SWAP_CHAIN_DESC	sd;
 	ZeroMemory(&sd, sizeof(sd));
 	sd.BufferCount = 1;	//백버퍼 갯수
@@ -80,8 +86,8 @@ HRESULT initDevice()
 		&g_pSwapChain,				//생성된 SwapChain
 		&g_pd3dDevice,				//생성된 device
 		&g_featureLevel,			//사용된 featireLevel
-		&g_pImmediateContex			//DC
-	); 
+		&g_pImmediateContext			//DC
+		);
 
 	if (FAILED(hr))
 	{
@@ -94,17 +100,17 @@ HRESULT initDevice()
 		0,
 		__uuidof(ID3D11Texture2D),	//버퍼 형식
 		(LPVOID*)&pBackBuffer		//받아온 버퍼
-	);
+		);
 
 	if (FAILED(hr))
 	{
 		return hr;
 	}
 
-	hr = g_pd3dDevice->CreateRenderTargetView(	pBackBuffer,
-												NULL, //자원 형식
-												&g_pRenderTargetView
-												);
+	hr = g_pd3dDevice->CreateRenderTargetView(pBackBuffer,
+		NULL, //자원 형식
+		&g_pRenderTargetView
+		);
 
 	pBackBuffer->Release();	//get 해왔으면 반드시 release
 
@@ -112,13 +118,13 @@ HRESULT initDevice()
 	{
 		return hr;
 	}
-		
-	g_pImmediateContex->OMSetRenderTargets(
+
+	g_pImmediateContext->OMSetRenderTargets(
 		1,						//렌더 대상 개수. 장면 분할시 1 초과
 		&g_pRenderTargetView,	//렌더 타겟
 		NULL					//깊이/스텐실 버퍼
-	);
-	
+		);
+
 	D3D11_VIEWPORT		vp;
 	vp.Width = 800;			//뷰포트 너비
 	vp.Height = 600;		//뷰포트 높이
@@ -127,49 +133,22 @@ HRESULT initDevice()
 	vp.TopLeftX = 0;		//그리기 시작 원점 x
 	vp.TopLeftY = 0;		//그리기 시작 원점 y
 
-	g_pImmediateContex->RSSetViewports(1, &vp); //레스터라이즈
+	g_pImmediateContext->RSSetViewports(1, &vp); //레스터라이즈
 
+	return S_OK;
 }
 
-void Render()
-{
-	//just clear the backbuffer
-	float clearColor[4] = { 0.8f, 0.15f, 0.5f, 0.5f }; // rgbm alpha >> 배경화면 초기화
-
-	//Clear
-	g_pImmediateContex->ClearRenderTargetView(g_pRenderTargetView, clearColor);
-
-	//////////////////////////////////////////////////////////////////////////
-	//폴리곤 그리는 부분
-	//////////////////////////////////////////////////////////////////////////
-
-	//set input assembler
-	g_pImmediateContex->IASetInputLayout(g_pVertexLayout);
-	g_pImmediateContex->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-
-	UINT stride = sizeof(MyVertex);
-	UINT offset = 0;
-	g_pImmediateContex->IASetVertexBuffers(0, 1, &g_pVertexBuffer, &stride, &offset);
-
-	//set Shader and Draw
-	g_pImmediateContex->VSSetShader(g_pVertexShader, NULL, 0);
-	g_pImmediateContex->PSSetShader(g_pPixelShader, NULL, 0);
-	g_pImmediateContex->Draw(6, 0);		//index를 쓰지 않는 그리기 함수
-
-	//Render (백버퍼를 프론트버퍼로 그린다.
-	g_pSwapChain->Present(0, 0); //다 그렸으니 즉시 교체
-}
 
 void CleanupDevice()
 {
+	if (g_pImmediateContext)
+	{
+		g_pImmediateContext->ClearState();
+	}
+
 	if (g_pRenderTargetView)
 	{
 		g_pRenderTargetView->Release();
-	}
-
-	if (g_pImmediateContex)
-	{
-		g_pImmediateContex->ClearState();
 	}
 
 	if (g_pSwapChain)
@@ -180,6 +159,27 @@ void CleanupDevice()
 	if (g_pd3dDevice)
 	{
 		g_pd3dDevice->Release();
+	}
+
+	if (g_pVertexBuffer)
+	{
+		g_pVertexBuffer->Release();
+	}
+
+	if (g_pVertexLayout)
+	{
+		g_pVertexLayout->Release();
+	}
+
+	if (g_pVertexShader)
+	{
+		g_pVertexShader->Release();
+	}
+
+
+	if (g_pPixelShader)
+	{
+		g_pPixelShader->Release();
 	}
 }
 
@@ -196,7 +196,7 @@ void CreateShader()
 		"VS", "vs_5_0",				//컴파일 설정
 		0, 0, 0,					//쉐이더 옵션
 		&pVSBlob, &pErrorBlob, 0	//return value
-	);
+		);
 
 	if (FAILED(hr))
 	{
@@ -204,11 +204,11 @@ void CreateShader()
 	}
 
 	hr = g_pd3dDevice->CreateVertexShader(
-		pVSBlob->GetBufferPointer(), 
-		pVSBlob->GetBufferSize(), 
-		0, 
+		pVSBlob->GetBufferPointer(),
+		pVSBlob->GetBufferSize(),
+		0,
 		&g_pVertexShader
-	);
+		);
 
 	if (FAILED(hr))
 	{
@@ -216,18 +216,19 @@ void CreateShader()
 	}
 
 	D3D11_INPUT_ELEMENT_DESC layout[] = {
-		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
 	};
 
 	UINT numElements = ARRAYSIZE(layout);
 
-	hr = g_pd3dDevice->CreateInputLayout( 
+	hr = g_pd3dDevice->CreateInputLayout(
 		layout,
 		numElements,
 		pVSBlob->GetBufferPointer(),
 		pVSBlob->GetBufferSize(),
 		&g_pVertexLayout
-	);
+		);
 
 	if (FAILED(hr))
 		return;
@@ -249,19 +250,19 @@ void CreateShader()
 
 	if (FAILED(hr))
 	{
-		return ;
+		return;
 	}
 
 	hr = g_pd3dDevice->CreatePixelShader(
 		pPSBlob->GetBufferPointer(),
 		pPSBlob->GetBufferSize(),
-		0, 
+		0,
 		&g_pPixelShader
-	);
+		);
 
 	if (FAILED(hr))
 	{
-		return ;
+		return;
 	}
 
 	pPSBlob->Release();
@@ -272,26 +273,24 @@ void CreateVertexBuffer()
 	//////////////////////////////////////////////////////////////////////////
 	//꼭지점 만들기
 	//////////////////////////////////////////////////////////////////////////
-	MyVertex vertices[] = {
-		{ XMFLOAT3(-0.5f, 0.5f, 1.0f)	}, //1
-		{ XMFLOAT3(0.5f, -0.5f, 1.0f)	}, //2
-		{ XMFLOAT3(-0.5f, -0.5f, 1.0f)	}, //3
 
-		{ XMFLOAT3(0.5f, -0.5f, 1.0f)	}, //2
-		{ XMFLOAT3(-0.5f, 0.5f, 1.0f)	}, //1
-		{ XMFLOAT3(0.5f, 0.5f, 1.0f)	}, //4
-		
-// 		{ XMFLOAT3(-0.5f, 0.5f, 1.0f)	},
-// 		{ XMFLOAT3(0.5f, 0.5f, 1.0f)	},
-// 		{ XMFLOAT3(0.5f, -0.5f, 1.0f)	},
+
+	MyVertex vertices[] =
+	{
+		{ XMFLOAT3(-0.5f, 0.5f, 1.0f), XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f) },
+		{ XMFLOAT3(0.5f, 0.5f, 1.0f), XMFLOAT4(0.0f, 1.0f, 0.0f, 1.0f) },
+		{ XMFLOAT3(0.5f, -0.5f, 0.0f), XMFLOAT4(0.0f, 0.0f, 1.0f, 1.0f) },
+		{ XMFLOAT3(-0.5f, -0.5f, 1.0f), XMFLOAT4(1.0f, 1.0f, 0.0f, 1.0f) },
+
 	};
 
+
 	D3D11_BUFFER_DESC		bd;
-	ZeroMemory(&bd, sizeof(bd));	
-	bd.ByteWidth		= sizeof(vertices);			//버퍼 크기
-	bd.Usage			= D3D11_USAGE_DEFAULT;		//버퍼 사용 방식
-	bd.BindFlags		= D3D11_BIND_VERTEX_BUFFER;	//파이프라인에 연결되는 버퍼 형태
-	bd.CPUAccessFlags	= 0;						//CPU 접근 flag. 일반적으로 GPU를 사용하기 때문에 0을 쓴다.
+	ZeroMemory(&bd, sizeof(bd));
+	bd.ByteWidth = sizeof(vertices);			//버퍼 크기
+	bd.Usage = D3D11_USAGE_DEFAULT;		//버퍼 사용 방식
+	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;	//파이프라인에 연결되는 버퍼 형태
+	bd.CPUAccessFlags = 0;						//CPU 접근 flag. 일반적으로 GPU를 사용하기 때문에 0을 쓴다.
 
 	D3D11_SUBRESOURCE_DATA	initData;
 
@@ -300,76 +299,128 @@ void CreateVertexBuffer()
 	initData.pSysMem = vertices;	//초기화하기 위한 버퍼 배열
 
 	HRESULT hr = g_pd3dDevice->CreateBuffer(&bd,	//생성할 버퍼의 정보를 담은 구조체
-								&initData,			//버퍼 초기화시 필요한 데이터
-								&g_pVertexBuffer	//생성된 버퍼
-								);
+		&initData,			//버퍼 초기화시 필요한 데이터
+		&g_pVertexBuffer	//생성된 버퍼
+		);
+
 	if (FAILED(hr))
 	{
 		return;
 	}
 }
 
-int APIENTRY WinMain( HINSTANCE hInstance,
-					  HINSTANCE hPrevInstance,
-					  LPSTR lpszCmdParam,
-					  int nCmdShow)
+void CreateIndexBuffer()
+{
+	UINT	indices[] =
+	{
+		0, 1, 2, //삼각형 0
+		0, 2, 3	 //삼각형 1
+	};
+
+
+	D3D11_BUFFER_DESC ibd;
+	ZeroMemory(&ibd, sizeof(ibd));
+	ibd.ByteWidth = sizeof(indices); //sizeof(uint)*6
+	ibd.Usage = D3D11_USAGE_IMMUTABLE; //cpu접근 불가 생성후 변경 불가. gpu만 접근 가능
+	ibd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	ibd.CPUAccessFlags = 0;
+
+	D3D11_SUBRESOURCE_DATA		initData;
+	ZeroMemory(&initData, sizeof(initData));
+	initData.pSysMem = indices;
+	g_pd3dDevice->CreateBuffer(&ibd, &initData, &g_pIndexBuffer);
+}
+
+void Render(float deltaTime)
+{
+	float ClearColor[4] = { 0.3f, 0.3f, 0.3f, 1.0f };
+	g_pImmediateContext->ClearRenderTargetView(g_pRenderTargetView, ClearColor);
+
+	// Set Input Assembler
+	g_pImmediateContext->IASetInputLayout(g_pVertexLayout);
+	g_pImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+
+	UINT stride = sizeof(MyVertex);
+	UINT offset = 0;
+	g_pImmediateContext->IASetVertexBuffers(0, 1, &g_pVertexBuffer, &stride, &offset);
+	g_pImmediateContext->IASetIndexBuffer(g_pIndexBuffer, DXGI_FORMAT_R32_UINT, 0);
+	
+	// Set Shader
+	g_pImmediateContext->VSSetShader(g_pVertexShader, NULL, 0);
+	g_pImmediateContext->PSSetShader(g_pPixelShader, NULL, 0);
+	
+	// Draw
+	g_pImmediateContext->DrawIndexed(6, 0, 0);
+
+
+	g_pSwapChain->Present(0, 0);
+}
+
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpszCmdParam, int nCmdShow)
 {
 	WNDCLASSEX wcex;
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = (WNDPROC)WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
+	wcex.hIconSm = LoadIcon(hInstance, IDI_APPLICATION);
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = szWindowClass;
 
-	wcex.cbSize			= sizeof(WNDCLASSEX);
-	wcex.style			= CS_HREDRAW | CS_VREDRAW;
-	wcex.lpfnWndProc	= (WNDPROC)WndProc;
-	wcex.cbClsExtra		= 0;
-	wcex.cbWndExtra		= 0;
-	wcex.hInstance		= hInstance;
-	wcex.hIcon			= LoadIcon(hInstance, IDI_APPLICATION);
-	wcex.hIconSm		= LoadIcon(hInstance, IDI_APPLICATION);
-	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
-	wcex.hbrBackground	= (HBRUSH)GetStockObject(WHITE_BRUSH);
-	wcex.lpszMenuName	= NULL;
-	wcex.lpszClassName	= szWindowClass;
-
-	if( !RegisterClassEx(&wcex) )
+	if (!RegisterClassEx(&wcex))
 		return 0;
 
-	HWND	hWnd = CreateWindowEx( WS_EX_APPWINDOW
-					, szWindowClass
-					, szTitle
-					, WS_OVERLAPPEDWINDOW
-					, CW_USEDEFAULT
-					, CW_USEDEFAULT
-					, 800
-					, 600
-					, NULL
-					, NULL
-					, hInstance
-					, NULL );
+	HWND	hWnd = CreateWindowEx(WS_EX_APPWINDOW
+		, szWindowClass
+		, szTitle
+		, WS_OVERLAPPEDWINDOW
+		, CW_USEDEFAULT
+		, CW_USEDEFAULT
+		, 800
+		, 600
+		, NULL
+		, NULL
+		, hInstance
+		, NULL);
 
-	if( !hWnd )
+	if (!hWnd)
 		return 0;
 
 	ShowWindow(hWnd, nCmdShow);
 
 	g_hWnd = hWnd;
+
 	initDevice();
 	CreateShader();
 	CreateVertexBuffer();
+	CreateIndexBuffer();
 
 	MSG			msg;
 
-	while( true )
+	while (true)
 	{
 		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
 		{
-			if (msg.message == WM_QUIT)
+			if (WM_QUIT == msg.message)
 				break;
-
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		}
 		else
 		{
-			Render();
+			static float deltaTime = 0;
+			deltaTime += 0.0002f;
+			// GetDeltaTime이 없으면 0.00005f등의 작은수를 쓸 것.
+
+			Render(deltaTime);
 		}
 	}
 
@@ -379,26 +430,26 @@ int APIENTRY WinMain( HINSTANCE hInstance,
 }
 
 // 메시지 처리 함수
-LRESULT CALLBACK WndProc( HWND hWnd
-						 , UINT message
-						 , WPARAM wParam
-						 , LPARAM lParam )
+LRESULT CALLBACK WndProc(HWND hWnd
+	, UINT message
+	, WPARAM wParam
+	, LPARAM lParam)
 {
 	HDC	hdc;
 	PAINTSTRUCT	ps;
 
-	switch(message)
+	switch (message)
 	{
 	case WM_CREATE:
 		break;
 
 	case WM_PAINT:
-		{
-			hdc = BeginPaint( hWnd, &ps );
+	{
+		hdc = BeginPaint(hWnd, &ps);
 
-			EndPaint( hWnd, &ps );
-		}
-		break;
+		EndPaint(hWnd, &ps);
+	}
+	break;
 
 	case WM_DESTROY:
 		PostQuitMessage(0);
